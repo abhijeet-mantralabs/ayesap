ede00308b661972f1b5d849cb9c2b9082906f927
abhijeet-mantralabs <abhijeet@mantralabsglobal.com>
2015-07-06 13:12:49 +0530

    polling done , email service work almost donne, created new files and routes for nearby resources

---
+++ api/api/controllers/ResourceController.js
@@ -0,0 +1,13 @@
+/**
+ * ResourceController
+ *
+ * @description :: Server-side logic for managing resources
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+    fetchNearResources : function(req, res){
+       console.log(req.body)
+    }
+};
+
--- client-admin/app/scripts/admin.js
+++ client-admin/app/scripts/admin.js
@@ -2,15 +2,13 @@
 // 'use strict';
 
 angular
-	.module('admin', [
-	'ngResource'
-	])
+	.module('admin', ['ngResource'])
 	// .config(['$httpProvider', function($httpProvider) {
  //        $httpProvider.defaults.useXDomain = true;
  //        delete $httpProvider.defaults.headers.common['X-Requested-With'];
  //    }
 	// ])
-	.controller('AdminCtrl', function ($scope, retailerAdmin, $interval) {
+	.controller('AdminCtrl', function ($scope, retailerAdmin, $interval, $timeout) {
 		console.log('inadmin');
 		
 
@@ -24,26 +22,45 @@ angular
 			// console.log(this.className);
 			// console.log($(this).parent().hasClass('retailer'));
 			$('a[href="#add-retailer"]').tab('show');
-			$scope.retailerRegister.name = retailerDetails.name;
-			$scope.retailerRegister.email = retailerDetails.email;
-			$scope.retailerRegister.mobile = retailerDetails.mobile;
-			$scope.retailerRegister.retailerId = retailerDetails.retailerId;
-			$scope.retailerRegister.retailerType = retailerDetails.retailerType;
-			$scope.retailerRegister.country = retailerDetails.country;
-			$scope.retailerRegister.state = retailerDetails.state;
-			$scope.retailerRegister.city = retailerDetails.city;
-			$scope.retailerRegister.area = retailerDetails.area;
-			$scope.retailerRegister.street = retailerDetails.street;
-			$scope.retailerRegister.address = retailerDetails.address;
-			$scope.retailerRegister.pincode = retailerDetails.pincode;
-		}
 
-		retailerAdmin.fetchRetailers().then(function(response){
-			$scope.retailers = response.details.retailerList;
-			console.log(response);
-		}).catch(function(err){
-			$scope.error = err.message;
-		})
+                $scope.retailerRegister = {
+                    name : retailerDetails.name,
+                    email : retailerDetails.email,
+                    mobile : retailerDetails.mobile,
+                    retailerId : retailerDetails.retailerId,
+                    retailerType : retailerDetails.retailerType,
+                    country : retailerDetails.country,
+                    state : retailerDetails.state,
+                    city : retailerDetails.city,
+                    area : retailerDetails.area,
+                    street : retailerDetails.street,
+                    address : retailerDetails.address,
+                    pincode : retailerDetails.pincode
+            }
+		}
+        var fetchRetailerList = function(){
+            retailerAdmin.fetchRetailers().then(function(response){
+                $scope.retailers = response.details.retailerList;
+                console.log(response);
+            }).catch(function(err){
+                $scope.error = err.message;
+            })
+        }
+        fetchRetailerList();
+        var poll = function() {
+            $timeout(function() {
+                fetchRetailerList();
+                poll();
+            }, 300000);
+        };
+        poll();
+
+//		retailerAdmin.fetchRetailers().then(function(response){
+//			$scope.retailers = response.details.retailerList;
+//			console.log(response);
+//		}).catch(function(err){
+//			$scope.error = err.message;
+//		})
 		// $interval(retailerAdmin.fetchRetailers().then(function(response){
 		// 	$scope.retailers = response.details.retailerList;
 		// 	console.log(response);
