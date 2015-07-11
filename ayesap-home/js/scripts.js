f97cf35a93f24861c478ac11944d2885cb3e2726
balakrishna@mantralabsglobal.com <balakrishna10@gmail.com>
2015-07-11 19:06:09 +0530

    changed the styles

--- ayesap-home/js/scripts.js
+++ ayesap-home/js/scripts.js
@@ -1,80 +1,111 @@
 window.aboutScroll = function(ele,el)
 {   
 
-  var section;
-   switch(ele)
-   {
-      case 'home':
-           section = '#home';                
-          break; 
+    var section;
+    switch(ele)
+    {
+        case 'home':
+            section = '#home';                
+            break; 
         
-      case 'how-it-works': 
-          section = '#how-it-works';
-          break; 
+        case 'how-it-works': 
+            section = '#how-it-works';
+            break; 
           
-      case 'start-shipping': 
-          section = '#get-started';
-          break; 
+        case 'start-shipping': 
+            section = '#get-started';
+            break; 
 
-      case 'become-rider':
-        section = '#become-rider';
-        break;
+        case 'become-rider':
+            section = '#become-rider';
+            break;
 
-      case 'contact-us':
-        section = "#contact-us";
-        break;
+        case 'contact-us':
+            section = "#contact-us";
+            break;
         
-      default : section = 'body';   
+        default : section = 'body';   
     }
-    // if($('.nav-container').height() == null){
-    //   var pos = $('.aboutushdr-fixed ').height() + 37;
-    // }else{
-      var pos = $('header').height();
-    // }
+    var pos = $('header').height();
   
-  $('html, body').animate({scrollTop:$(section).offset().top-pos},1000);
+    $('html, body').animate({scrollTop:$(section).offset().top-pos},1000);
 }
 
 
-   $(window).scroll(function (event)
-   {
+$(window).scroll(function (event){
+   
+    var home = $('#home').height()-$('header').height();
+    var howitworks = $('#how-it-works').offset().top-$('header').height()-5;
+    var getstarted =  $('#get-started').offset().top-$('header').height()-5;
+    var becomerider = $('#become-rider').offset().top-$('header').height()-5;
+    var contactus = $('#contact-us').offset().top-$('header').height()-5; 
 
-      var home = $('#home').height()-$('header').height();
-      var howitworks = $('#how-it-works').offset().top-$('header').height()-5;
-      var getstarted =  $('#get-started').offset().top-$('header').height()-5;
-      var becomerider = $('#become-rider').offset().top-$('header').height()-5;
-      var contactus = $('#contact-us').offset().top-$('header').height()-5;  
-
-      if ($(window).scrollTop() <= howitworks){
-        $('.nav-item').removeClass('active-tab');
-        $('#nav-home').addClass('active-tab');
-      }
-      else if ($(window).scrollTop() <= getstarted && ($(window).scrollTop() >= home)){
-        $('.nav-item').removeClass('active-tab');
-        $('#nav-how-it-works').addClass('active-tab');
-      }
-      else if ($(window).scrollTop() <= becomerider && ($(window).scrollTop() >= getstarted)){
-        $('.nav-item').removeClass('active-tab');
-        $('#nav-start-shipping').addClass('active-tab');
-      }
-      else if ($(window).scrollTop() <= contactus && ($(window).scrollTop() >= becomerider)){
-        $('.nav-item').removeClass('active-tab');
-        $('#nav-become-rider').addClass('active-tab');
-      }
-      else if ($(window).scrollTop() >= becomerider){
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
         $('.nav-item').removeClass('active-tab');
         $('#nav-contact-us').addClass('active-tab');
-      }  
-  });
+    }
+    else if ($(window).scrollTop() <= howitworks){
+        $('.nav-item').removeClass('active-tab');
+        $('#nav-home').addClass('active-tab');
+    }
+    else if ($(window).scrollTop() <= getstarted && ($(window).scrollTop() >= home)){
+        $('.nav-item').removeClass('active-tab');
+        $('#nav-how-it-works').addClass('active-tab');
+    }
+    else if ($(window).scrollTop() <= becomerider && ($(window).scrollTop() >= getstarted)){
+        $('.nav-item').removeClass('active-tab');
+        $('#nav-start-shipping').addClass('active-tab');
+    }
+    else if ($(window).scrollTop() <= contactus && ($(window).scrollTop() >= becomerider)){
+        $('.nav-item').removeClass('active-tab');
+        $('#nav-become-rider').addClass('active-tab');
+    }
+    else if ($(window).scrollTop() >= becomerider){
+        $('.nav-item').removeClass('active-tab');
+        $('#nav-contact-us').addClass('active-tab');
+    }
 
-$(".screenshot").hover(function () {
-  console.log();
-  $(this).children('.screenshot-cover').slideToggle();
 });
 
-var formData = {
-    name:$('#name').val(),
-    mobile: $('#phone-number').val(),
-    city:$('#city').val(),
-    comment:$('#comment').val()
+$(".screenshot").hover(function () {
+    $(this).children('.screenshot-cover').slideToggle();
+});
+
+window.contactRetailer = function(){
+    var base_url = "http://ayesap.zolome.com/api/";
+    console.log('in');
+    var formData = {
+        name:$('#name').val(),
+        mobile: $('#phone-number').val(),
+        city:$('#city').val(),
+        comment:$('#comment').val()
+    }
+    $.ajax({
+        url: base_url+'retailer/requestForRegister', formData,
+        type: 'post',
+        dataType: 'json',
+        data:formData,
+        success: function(data) {
+            // alert('success');
+        }
+    });
 }
+window.becomeRider = function(){
+    var base_url = "http://ayesap.zolome.com/api/";
+    console.log('in');
+    var formData = {
+        name:$('#rider-name').val(),
+        mobile: $('#rider-phone').val(),
+        city:$('#rider-city').val(),
+    }
+    $.ajax({
+        url: base_url+'resources/reqForResRegister', formData,
+        type: 'post',
+        dataType: 'json',
+        data:formData,
+        success: function(data) {
+            // alert('success');
+        }
+    });
+}
+
