99318d822342fd27dc3cc72d3aec69071fa0c7ee
balakrishna@mantralabsglobal.com <balakrishna10@gmail.com>
2015-07-13 13:49:39 +0530

    Did the modifications in html

--- ayesap-home/js/scripts.js
+++ ayesap-home/js/scripts.js
@@ -1,10 +1,10 @@
-if($(window).innerHeight()>768){
+// if($(window).innerHeight()>768){
     $('.slider-content').height($(window).innerHeight()-$('header').height());
     // console.log();
     $(window).resize(function(event) {
         $('.slider-content').height($(window).innerHeight()-$('header').height());
     });
-}
+// }
 window.aboutScroll = function(ele,el)
 {   
 
