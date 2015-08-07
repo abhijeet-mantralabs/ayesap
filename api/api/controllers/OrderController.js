2cc6b6fe94210a8b795ee11464a932733a886ccd
abhijeet-mantralabs <abhijeet@mantralabsglobal.com>
2015-08-07 13:49:36 +0530

    order status checkin added

--- api/api/controllers/OrderController.js
+++ api/api/controllers/OrderController.js
@@ -271,8 +271,11 @@ module.exports = {
         });
     },
     getOrderStatus: function(req, res){
+        console.log("order completed updated original------>>>")
+        console.log(req.body);
         console.log("order completed updated------>>>")
         console.log(JSON.stringify(req.body))
+
         var latestOrderStatus = req.body;
 
 
