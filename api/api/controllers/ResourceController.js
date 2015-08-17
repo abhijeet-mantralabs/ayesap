8f184bb7a720b66e02ad4af4f63553b0fd56ca14
abhijeet-mantralabs <abhijeet@mantralabsglobal.com>
2015-08-17 15:02:58 +0530

    time object format changed

--- api/api/controllers/ResourceController.js
+++ api/api/controllers/ResourceController.js
@@ -450,7 +450,7 @@ var difference = function(array){
 };
 
 function convertDateToISO(datetime){
-    var match = datetime.match(/^(\d+)-(\d+)-(\d+) (\d+)\:(\d+)\:(\d+)\:(\d+)$/)
+    var match = datetime.match(/^(\d+)-(\d+)-(\d+) (\d+)\:(\d+)\:(\d+)\.(\d+)$/)
     var updateISOTime = new Date(match[1], match[2] - 1, match[3], match[4], match[5], match[6], match[7])
     return updateISOTime;
 }
